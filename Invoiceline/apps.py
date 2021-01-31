2a071b6360e7163ceebc4064d63e4ba19bed5321
siheyong <tlgud1013@gamil.com>
2021-01-31 23:31:54 +0900

    pratice model

---
+++ Invoiceline/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InvoicelineConfig(AppConfig):
+    name = 'Invoiceline'
---
+++ Invoiceline/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Todo(modesl.Model):
+    id = models.IntegerField(default=0)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
